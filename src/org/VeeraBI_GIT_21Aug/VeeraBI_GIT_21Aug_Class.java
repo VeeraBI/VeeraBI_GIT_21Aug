7e994c4ef7b8378590607bd0984b134c83621f21
VeeraBI <tomyganesan@gmail.com>
2022-08-21 18:05:21 +0530

    Second Push

--- src/org/VeeraBI_GIT_21Aug/VeeraBI_GIT_21Aug_Class.java
+++ src/org/VeeraBI_GIT_21Aug/VeeraBI_GIT_21Aug_Class.java
@@ -3,7 +3,8 @@ package org.VeeraBI_GIT_21Aug;
 public class VeeraBI_GIT_21Aug_Class {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Veera");
 
 	}
 
